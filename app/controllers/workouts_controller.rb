345d551ce2b690b3bfaf6a8dd7c888760edbdbc9
MaximillianBrain <max_brain@ymail.com>
2016-12-14 12:00:12 -0800

    Added Workout Controller Functionality

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,54 @@
+class WorkoutsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index]
+
+  def index
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def showall
+    @workouts = Workout.all.order('created_at DESC')
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(post_params)
+
+    if @workout.save # checks if workout is valid
+      redirect_to @workout
+    else
+      render 'new'  #renders new method if it is not valid
+      end
+  end
+
+  def delete
+    @workout = Workout.find(params[:id])
+    @workout.destroy
+
+    redirect_to root_path
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+
+    if @workout.update(params[:workout].permit(:time_taken, :time, :lenght))
+      redirect_to @workout
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:workout).permit(:time_taken, :time, :lenght)
+  end
+end
